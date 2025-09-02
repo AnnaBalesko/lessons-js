f7582a5930e51af20903360ccc35a986d9decc6f
Anna Balesko <babasheldona@icloud.com>
2025-09-01 21:25:55 -1100

    add task

---
+++ js/lessons3.js
@@ -0,0 +1,25 @@
+// const friends = [
+//   { name: "Ross", online: false },
+//   { name: "Joey", online: true },
+//   { name: "Chandler", online: false },
+//   { name: "Phoebe", online: true },
+//   { name: "Monica", online: true },
+//   { name: "Rachel", online: false },
+// ];
+
+
+// function findByName(arr, friendName) {
+//     for (const item of arr) {
+//         console.log(item.name);
+        
+//     }
+// }
+// console.log(findByName(friends, "Joey"));
+
+
+// const stones = [
+//   { name: "Смарагд", price: 1300, quantity: 4 },
+//   { name: "Діамант", price: 2700, quantity: 3 },
+//   { name: "Сапфір", price: 400, quantity: 7 },
+//   { name: "Щебінь", price: 200, quantity: 2 },
+// ];
--- js/task.js
+++ js/task.js
@@ -241,5 +241,342 @@
 // console.log(getExtremeScores([89, 64, 42, 17, 93, 51, 26]));
 
 
+// колбек-функції=============================
+
+// function greet(name) {
+//     return `Welcome ${name}!`;
+// }
+// console.log(greet("Mango"));
+// console.log(greet);
+
+// function makePizza() {
+//    return "Your pizza is being prepared, please wait.";
+// }
+
+// const result = makePizza();
+// const pointer = makePizza;
+
+// console.log(result);
+// console.log(pointer);
+
+// function greet(name) {
+//     console.log(`Welcome ${name}!`);
+// }
+
+// function notify(name) {
+//     console.log(`Dear ${name}, your room will be ready in 30 minutes`);
+// }
+
+// function registerGuest(name, callback) {
+//     console.log(`Register ${name}`);
+//     callback(name);
+// }
+
+// registerGuest("Mango", greet);
+// registerGuest("Mango", notify);
+
+
+// function makeMessage(pizzaName, callback) {
+//     return callback(pizzaName);
+// }
+
+// function deliverPizza(pizzaName) {
+//     console.log(
+//     `Delivering ${pizzaName} pizza.`);
+// }
+
+// function makePizza(pizzaName) {
+//     console.log(
+//     `Pizza ${pizzaName} is being prepared, please wait...`);
+// }
+
+
+// makeMessage("Royal Grand", deliverPizza);
+// makeMessage("Ultracheese", makePizza);
+
+// інлайн=колбеки===============================
+// function registerGuest(name, callback) {
+//     console.log(`Registering ${name}!`);
+//     callback(name);
+// }
+
+// registerGuest("Mango", function greet(name) {
+//     console.log(`Welcome ${name}!`);
+// })
+
+// registerGuest("Polly", function notify(name) {
+//     console.log(`Dear ${name}, your room will be ready in 30 minutes`);
+// })
+
+// function makePizza(pizzaName, callback) {
+//     console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+//     callback(pizzaName);
+// }
+
+// makePizza("Royal Grand", function deliverPizza(pizzaName) {
+//     console.log(`Delivering pizza ${pizzaName}`);
+    
+// });
+
+// makePizza("Ultracheese", function eatPizza(pizzaName) {
+//     console.log(`Eating pizza ${pizzaName}`);
+// });
+
+//============forEach
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// for (let i = 0; i < numbers.length; i += 1){
+//     console.log(`Index ${i}, value ${numbers[i]}`);
+// }
+
+// numbers.forEach(function (number, index) {
+//     console.log(`Index ${index}, value ${number}`);
+
+// });
+
+
+// function calculateTotalPrice(orderedItems) {
+//     let totalPrise = 0;
+//     orderedItems.forEach(function (number) {
+//         totalPrise = totalPrise + number;
+//     });
+//     return totalPrise;
+// }
+// console.log(
+//  calculateTotalPrice([412, 371, 94, 63, 176]));
+
+// function filterArray(numbers, value) {
+//     const newArray = [];
+//     numbers.forEach(function (number) {
+//         if (number > value) {
+//             newArray.push(number);
+//         };
+//     });
+//     return newArray;
+// }
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+
+
+//===========стрілочні функції
+
+// function classicAdd(a, b, c) {
+//     return a + b + c;
+// }
+
+// const arrowAdd = (a, b, c) => {
+//     return a + b + c;
+// };
+
+// const addO = a => {
+//     return a + 5;
+// };
+
+// const add = () => {
+//     console.log("Hello!");
+// };
+
+// const calculateTotalPrice = (quantity, pricePerItem) => {
+//     return quantity * pricePerItem;
+// };
+
+// console.log(calculateTotalPrice(5, 100));
+
+
+// const add = (a, b) => a + b;
+
+// console.log(add(4, 5));
+
+// const calculateTotalPrice = (quantity, pricePerItem) => quantity * pricePerItem;
+// console.log(calculateTotalPrice(5, 100));
+
+// const add = (...args) => {
+//     console.log(args);
+// }
+
+// add(1, 2, 3);
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// numbers.forEach(function (number, index) {
+//     console.log(`Index ${index}, value ${number}`);
+// });
+
+// numbers.forEach((number, index) => {
+//     console.log(`Index ${index}, value ${number}`);
+// });
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// const logMessage = (number, index) => {
+//     console.log(`Index ${index}, value ${number}`);
+// };
+
+// numbers.forEach(logMessage);
+
+// const calculateTotalPrice = (orderedItems) => {
+//     let totalPrice = 0;
+//     orderedItems.forEach((item) => {
+//         totalPrice += item;
+//     });
+//     return totalPrice;
+// }
+// console.log(
+//  calculateTotalPrice([12, 85, 37, 4]));
+
+// const filterArray = (numbers, value) => {
+//     const filteredNumbers = [];
+//     numbers.forEach((number) => {
+//         if (number > value) {
+//             filteredNumbers.push(number);
+//         }
+//     });
+//     return filteredNumbers;
+// }
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+
+
+//====================map i flatMap
+
+// const dirtyMultiply = (array, value) => {
+//     for (let i = 0; i < array.length; i += 1) {
+//         array[i] = array[i] * value;
+//     }
+// };
+
+// const numbers = [1, 2, 3, 4, 5];
+// dirtyMultiply(numbers, 2);
+
+// console.log(numbers);
+
+// const pureMultiply = (array, value) => {
+//     const newArray = [];
+//     array.forEach(element => {
+//         newArray.push(element * value);
+//     });
+//     return newArray;
+// }
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const doubledNumbers = pureMultiply(numbers, 2);
+
+// console.log(numbers);
+
+// console.log(doubledNumbers);
+
+// function changeEven(numbers, value) {
+//     const newArray = [];
+//   numbers.forEach(number => {
+//     if (number % 2 === 0) {
+//     newArray.push(number + value);
+//       } else { newArray.push(number) };
+//   });
+//     return newArray;
+// };
+
+// console.log(changeEven([1, 2, 3, 4, 5], 10));
+
+// ==========перебираючі методи
+//=============метод мар()
+
+// const planets = ["Earth", "Mars", "Venus", "Jupiter"];
+
+// const planetsInUpperCase = planets.map(planet => planet.toLocaleUpperCase());
+
+// console.log(planetsInUpperCase);
+// console.log(planets);
+
+// const planetsInLowerCase = planets.map(planet => planet.toLocaleLowerCase());
+
+// console.log(planetsInLowerCase);
+// console.log(planets);
+
+// const planets = ["Earth", "Mars", "Venus", "Jupiter"];
+
+// const planetsLengths = planets.map(planet => planet.length);
+
+// console.log(planets);
+// console.log(planetsLengths);
+
+// const students = [
+//   { name: "Mango", score: 83 },
+//   { name: "Poly", score: 59 },
+//   { name: "Ajax", score: 37 },
+//   { name: "Kiwi", score: 94 },
+//   { name: "Houston", score: 64 },
+// ];
+// for (const student of students) {
+//     console.log(`${student.name}:${student.score}`);
+// }
+// const names = students.map(student => student.name);
+
+// console.log(names);
+
+// const values = students.map(value => value.score);
+
+// console.log(values);
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Tell-Tale Heart",
+//     author: "Edgar Allan Poe",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
+
+// const titles = books.map((book) => book.title);
+
+// console.log(titles);
+
+//=============flatMap()
+// const students = [
+//   { name: "Mango", courses: ["mathematics", "physics"] },
+//   { name: "Poly", courses: ["science", "mathematics"] },
+//   { name: "Kiwi", courses: ["physics", "biology"] },
+// ];
+
+// const mappedCourses = students.map(student => student.courses);
+
+// console.log(mappedCourses);
+
+// const flattenedCourses = students.flatMap(student => student.courses);
+
+// console.log(flattenedCourses);
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     genres: ["adventure", "history"],
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     genres: ["fiction"],
+//   },
+//   {
+//     title: "Redder Than Blood",
+//     author: "Tanith Lee",
+//     genres: ["horror", "mysticism"],
+//   },
+// ];
+
+// const genres = books.flatMap(book => book.genres);
+
+// console.log(genres);
 
 
