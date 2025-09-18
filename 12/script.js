060c2271953640527940cbb8b645b9e065b8a3c6
Anna Balesko <babasheldona@icloud.com>
2025-09-17 18:32:14 -1100

    add lessons

---
+++ 12/script.js
@@ -0,0 +1,82 @@
+//=====================quertyselector
+
+
+// const btn = document.querySelector("#magic-btn");
+
+// const navItem = document.querySelector(".site-nav__item");
+
+// const nav = document.querySelector("ul");
+
+// const navLinksItem = document.querySelectorAll(".site-nav__item");
+
+// const navSite = document.querySelector(".site-nav");
+
+// // console.log(navSite.lastElementChild);
+// // console.log(navSite.firstElementChild);
+// // console.log(navSite.childNodes);
+// // console.log(navSite.children);
+
+// const portfolioEl = navSite.children[1];
+// // console.log(portfolioEl.previousElementSibling);
+// // console.log(portfolioEl.nextElementSibling);
+// console.log(portfolioEl.parentNode);
+
+
+//========================img
+
+
+// console.log(imgEl.alt);
+// imgEl.alt = "Cat";
+// console.log(imgEl.alt);
+
+
+// const heroTitle = document.querySelector(".hero__title");
+// // console.log(heroTitle.textContent);
+// heroTitle.textContent = "hello, lalala";
+// console.log(heroTitle.textContent);
+
+// const imgEl = document.querySelector(".hero__img");
+//  imgEl.src = "https://d2zp5xs5cp8zlg.cloudfront.net/image-79322-800.jpg";
+// console.log(imgEl.getAttribute("src"));
+
+
+// imgEl.alt = "lalala";
+// imgEl.setAttribute("alt", "tototo");
+// console.log(imgEl);
+// imgEl.setAttribute("width", 200);
+// imgEl.removeAttribute("width");
+// console.log(imgEl.hasAttribute("href"));
+
+
+//=====================================data-atr
+
+const actions = document.querySelectorAll(".actions button");
+
+// console.log(actions[0].dataset.actions);
+
+// actions[0].dataset.actions = "lalala";
+
+// console.log(actions[0].dataset.actions);
+
+// actions[0].dataset.btn = "red";
+// console.log(actions[0].dataset.btn);
+
+// delete actions[0].dataset.btn;
+
+const currentPageUrl = '/contacts';
+
+// const navLinkEl = document.querySelector(`.site-nav__link[href="/contacts"]`);
+// const navLinkEl = document.querySelector(`.site-nav__link[href="${currentPageUrl}"]`);
+// console.log(navLinkEl.textContent);
+
+// navLinkEl.classList.add("site-nav__link--current");
+
+// console.log(navLinkEl);
+
+// navLinkEl.classList.remove("site-nav__link--current");
+
+// navLinkEl.classList.toggle("site-nav__link--current");
+
+// navLinkEl.classList.replace("site-nav__link--current", "blue");
+
+// console.log(navLinkEl.classList.contains("blue"));
